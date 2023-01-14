ad093bf7a99ce0aeccdb6ee434cb835ca0291cf1
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-01-14 13:18:16 -0500

    Update ObjectClick.cs

--- My project/Assets/ObjectClick.cs
+++ My project/Assets/ObjectClick.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class ObjectClick : MonoBehaviour
 {
     
-    float distractionTime;
-    
+    public float distractionTime = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +23,16 @@ public class ObjectClick : MonoBehaviour
     {
         // t$$anonymous$$s object was clicked - do somet$$anonymous$$ng
         Debug.Log("Object was clicked");
+        StartCoroutine(Test());
         // Destroy (t$$anonymous$$s.gameObject);
     } 
+
+    IEnumerator Test()
+    {
+        /* STOP PROGRESS BAR HERE */
+        yield return new WaitForSeconds(5f);
+        
+        /* RESUME PROGRESS BAR HERE */
+        Debug.Log("Object click expired.");
+    }
 }
